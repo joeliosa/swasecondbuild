28b769189a1f07b69afe7bee62927574eb6ba317
joel saarinen <40738921+joeliosa@users.noreply.github.com>
2020-08-18 14:35:24 +0300

    Add files via upload

---
+++ flask-backend/app.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask
+
+app = Flask(__name__, static_folder="/Users/joelsaarinen/projects/swabuildtwo/flask-backend/static")
+
+@app.route("/")
+def my_index():
+    return flask.render_template("index.html", token="Hello there!")
+
+app.run(debug=True)    
---
+++ flask-backend/static/react/precache-manifest.72d8719ac0c1211d50fc185cd8498be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23924ffa8ca6d542438e8a43b475ce6",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "78b28df065b47183e7e0",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "870f8938f671fcc758d7",
+    "url": "/static/react/js/2.992fbd94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.992fbd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b28df065b47183e7e0",
+    "url": "/static/react/js/main.493da43e.chunk.js"
+  },
+  {
+    "revision": "f1fb285ff5334e1d927c",
+    "url": "/static/react/js/runtime-main.7f1cfca5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
